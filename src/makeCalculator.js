3c3de5450000bd531dac8bfb3eaa3752a31b5cb8
Natalia Lunkevych <gadyka08@gmail.com>
2026-03-01 10:27:08 +0200

    vtyr

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,34 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+  //місце де зберігаємо поточне число
+  result: 0,
+    add: (current, num) => current + num,
+    subtract: (current, num) => current - num,
+    multiply: (current, num) => current * num,
+    divide: (current, num) => {
+      if (num === 0) {
+        console.log("Помилка!на нуль ділити не можна!");
+        return current;
+      }
+      return current / num;
+    },
+
+    // Викликаємо колбек, та передаємо йому поточний result та нове число.
+    operate(callback, num) {
+      this.result = callback(this.result, num);
+
+      return this;
+    },
+
+
+    reset() {
+      this.result = 0;
+      return this;
+    }
+  };
 }
+
 
 module.exports = makeCalculator;
